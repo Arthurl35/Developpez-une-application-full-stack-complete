9fee6b48e1d6bbc3b974f778b0d6c346fbb42a0c
arthur levesque <arthurlevesque35@gmail.com>
2024-02-26 19:14:28 +0100

    Modification du style de la première page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { MatCardModule } from '@angular/material/card';
 //import { FlexLayoutModule } from '@angular/flex-layout';
 //import { MeComponent } from './components/me/me.component';
+import {HomeComponent} from "./pages/home/home.component";
 
 const materialModule = [
   MatButtonModule,
---
+++ front/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    // paths to your content files
+    './src/**/*.html',
+    './src/**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
